472cef570aa1532ba7b20f467a04ec030c61bdd7
retrolemon <86541189+retrolemon@users.noreply.github.com>
2021-06-26 22:24:42 -0500

    Fixed some bad code and optimized screen clearing

--- console.py
+++ console.py
@@ -1,11 +1,10 @@
 from passgen import passgen
 from os import system
-from shutil import rmtree
-from json import loads
+import json
 
 with open("config.json") as jso:
     data = jso.read()
-    jsodct = loads(data)
+    jsodct = json.loads(data)
     jso.close()
 
 os = jsodct["os"]
@@ -18,8 +17,17 @@ length = jsodct["length"]
 charset = jsodct["charset"]
 
 while True:
-    rmtree("__pycache__")
+    with open("config.json", "w") as jso:
+        jso.write(json.dumps(jsodct))
+        jso.close()
+    
     i = input("passgen>")
     if i == "gen":print(passgen(length, charset))
     elif i == clear:system(clear)
+    elif i == "win":
+        jsodct["os"] = "win"
+    elif i == "unix":
+        jsodct["os"] = "unix"
+    elif i == "else":
+        jsodct["os"] = "else"
     else:continue
