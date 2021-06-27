f71f2bbd074acca8d80aa5d90ff6fce3e2a5f118
retrolemon <86541189+retrolemon@users.noreply.github.com>
2021-06-27 08:54:35 -0500

    Shortened the code and removed bugs

--- console.py
+++ console.py
@@ -1,33 +1,19 @@
 from passgen import passgen
 from os import system
 import json
-
-with open("config.json") as jso:
-    data = jso.read()
-    jsodct = json.loads(data)
-    jso.close()
-
-os = jsodct["os"]
-
-if os == "win":clear = "cls"
-elif os == "unix":clear = "clear"
-else:clear = "clear"
-
-length = jsodct["length"]
-charset = jsodct["charset"]
+import sys
 
 while True:
-    with open("config.json", "w") as jso:
-        jso.write(json.dumps(jsodct))
+    with open("config.json") as jso:
+        data = jso.read()
+        jsodct = json.loads(data)
         jso.close()
+
+    length = jsodct["length"]
+    charset = jsodct["charset"]
     
     i = input("passgen>")
     if i == "gen":print(passgen(length, charset))
-    elif i == clear:system(clear)
-    elif i == "win":
-        jsodct["os"] = "win"
-    elif i == "unix":
-        jsodct["os"] = "unix"
-    elif i == "else":
-        jsodct["os"] = "else"
+    elif i == clear:system("clear")
+    elif i == "exit":sys.exit()
     else:continue
